98487f6bcc0228509003b1aca81e44bf5b17f397
Kavia AI <production@kavia.ai>
2026-02-09 10:27:20 +0000

    Checkpoint 1 - cg62264940

---
+++ kyc_service/src/main/java/com/example/demo/kycserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.kycservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class kycserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(kycserviceApplication.class, args);
+	}
+
+}
